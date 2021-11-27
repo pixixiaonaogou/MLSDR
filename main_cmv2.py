2ed0f56399d48a11c3cee7d139e302c14e2f78ec
pixixiaonaogou <34858642+pixixiaonaogou@users.noreply.github.com>
2021-11-28 00:05:03 +0800

    Update main_cmv2.py

--- main_cmv2.py
+++ main_cmv2.py
@@ -290,8 +290,6 @@ if __name__ == '__main__':
     deterministic = True
     if deterministic:
         random_seeds = 137
-    else: 
-        random_seeds = np.random.randit(1,1000000)
     rounds = 1
     lr = 3e-5
     epochs = 250
@@ -300,7 +298,8 @@ if __name__ == '__main__':
     train_dataloader, val_dataloader = generate_dataloader(shape, batch_size, num_workers, data_mode)
     
     for i in range(rounds):
-        set_seed(random_seeds + i)
+        if deterministic:
+            set_seed(random_seeds + i)
       # create logger
         log, out_dir = CraateLogger(mode, model_name,i,data_mode)
         net = FusionNet(class_list).cuda()
